9d3f6670a3b73f6a47ad4e6c7eb6bf4a2b32750b
Diana Ozemebhoya Eromosele <diana.ozemebhoya@gmail.com>
2016-09-16 22:40:48 -0400

    Update interests_controller.rb

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,10 +1,7 @@
 class InterestsController < ApplicationController
 
   def show
-  	 # @interest = Interest.find_by(id: params[:id], politician_id: params[:politician_id])
-
   	@interest = Interest.find_by(id: params[:id])
-    # @politician = Politician.friendly.find(params[:politician_id])
     @politician = Politician.friendly.find(params[:politician_name])
     @issue = Issue.friendly.find(params[:id])
 
